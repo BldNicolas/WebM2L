cfe9c5feb1608e3c302fd86da4a219785888cca2
Nicolas <ni.berlaud@gmail.com>
2024-04-11 21:54:42 +0200

    ✨ feature: View the chirps in the chrip section

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -15,7 +15,9 @@ class ChirpController extends Controller
      */
     public function index(): View
     {
-        return view('chirps.index');
+        return view('chirps.index', [
+            'chirps' => Chirp::with('user')->latest()->get(),
+        ]);
     }
 
     /**
